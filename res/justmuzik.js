9a4963c1aa12c0193a8dc3d5f650d1d42c8077c3
chakrikodali <chakrikodali@gmail.com>
2011-09-22 00:25:57 -0700

    added moustache adn built first backbone app

--- res/justmuzik.js
+++ res/justmuzik.js
@@ -1,13 +1,5 @@
 $(function() {
-	//var muzik = new Muzik();
-	window.s = new Songs;
-	s.url = 'http://local.yahooapis.com/LocalSearchService/V3/localSearch?appid=YahooDemo&query=pizza&zip=10504&results=2&output=json&callback=?';
-	s.fetch({success: function(collection, data) {
-		console.log(arguments);
-	}, 
-	error: function() {
-		console.log(arguments);
-	}});
+	window.muzik = new Muzik;
 });
 
 var Song = Backbone.Model.extend({
@@ -18,14 +10,91 @@ var Song = Backbone.Model.extend({
 		return {
 			playing: false
 		}
+	},
+	play: function() {
+		alert('play');
+	},
+	pause: function() {
+		alert('pause');
+	},
+	replay: function() {
+		alert('replay');
 	}
 });
 
 var Songs = Backbone.Collection.extend({
 	model: Song,
+	
 	initialize: function() {
+		this.current = -1;
+		this.repeat = true;
+		this.shuffle = false;
+		this.yetToShuffle = [];	
+	},
+	url: function() {
+		return 'res/tmpdata.js';
+	},
+	next: function() {
+		var n;
+		if(!this.shuffle) {
+			 n = this.current + 1;
+		} else {
+			if(!this.yetToShuffle.length) {
+				this.yetToShuffle = this.models;
+			}
+			n = getRandomSong(this.yetToShuffle.length);
+			//this.yetToShuffle.remove(n);
+		}
 		
+		function getRandomSong(length) {
+			return Math.random()*length;
+		}
+		return n;
+	},
+	isRepeating: function() {
+		return repeat;
+	},
+	play: function() {
+		this.current = next();
+		//get model and call play on model
 	}
 });
 
+var SongView = Backbone.View.extend({
+	tagName : 'li',
+	
+	initialize: function() {
+		$(this.el).addClass('song');
+	},
+	render: function() {
+		//TODO: use mosutache templating engine to do this
+		$(this.el).html('<span class="playBtn"></span><span class="replayBtn"></span><span class="title"></span>');
+		this.$('.title').text(this.model.get('title'));
+		return this;
+	}, 
+	events: {
+		'click' : 'play',
+		'click .playBtn': 'play',
+		'click .replayBtn': 'replay'
+	},
+	play: function() {
+		this.model.play();
+	},
+	replay: function() {
+		this.model.replay();
+	}
+});
 
+var Muzik = Backbone.View.extend({
+	initialize: function() {
+		var songs = new Songs;
+		songs.fetch({dataType: 'json', success: function() {
+			songs.each(muzik.addSongView);			
+		}
+		});	
+	},
+	addSongView: function(song) {
+		var s = new SongView({model: song});
+		$('.songs .list').append(s.render().el);
+	}
+});
---
+++ res/tmpdata.js
@@ -0,0 +1,18 @@
+[{
+	"title": "Gumm Summ Gumm Gumm Summ",
+	"length": 120000,
+	"format": "mp4",
+	"location": "test1.mp4",
+	"albumId": 1,
+	"id":1,
+	"addedon": "22-sept"
+},
+{
+	"title": "Udhi Udhi Iteffaq Se",
+	"length": 120000,
+	"format": "ogg",
+	"location": "test2.mp4",
+	"albumId": 1,
+	"id":2,
+	"addedon": "22-sept"
+}]
